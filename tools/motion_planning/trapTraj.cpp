8b5aa7c906493fb0224ca773cd988e1914ec4432
Paul Guenette <pfguenette@gmail.com>
2019-06-03 21:00:52 +0200

    Add a bunch of stuff for testing with CSV

--- tools/motion_planning/trapTraj.cpp
+++ tools/motion_planning/trapTraj.cpp
@@ -44,7 +44,7 @@ bool TrapezoidalTrajectory::planTrapezoidal(float Xf, float Xi, float Vi,
     // aka "double deceleration move" in the paper
     if ((s * Vi) > (s * Vr_))
     {
-        cout << "Handbrake!" << endl;
+        // cout << "Handbrake!" << endl;
         Ar_ = -s * Amax;
     }
 
@@ -59,7 +59,7 @@ bool TrapezoidalTrajectory::planTrapezoidal(float Xf, float Xi, float Vi,
     // Are we displacing enough to reach cruising speed?
     if (s * dX < s * dXmin)
     {
-        cout << "Short Move: " << endl;
+        // cout << "Short Move: " << endl;
         // Short move (triangle profile)
         Vr_ = s * sqrtf((Dr_ * SQ(Vi) + 2 * Ar_ * Dr_ * dX) / (Dr_ - Ar_));
         Ta_ = std::max(0.0f, (Vr_ - Vi) / Ar_);
@@ -68,7 +68,7 @@ bool TrapezoidalTrajectory::planTrapezoidal(float Xf, float Xi, float Vi,
     }
     else
     {
-        cout << "Long Move: " << endl;
+        // cout << "Long Move: " << endl;
         // Long move (trapezoidal profile)
         Tv_ = (dX - dXmin) / Vr_;
     }
@@ -80,11 +80,11 @@ bool TrapezoidalTrajectory::planTrapezoidal(float Xf, float Xi, float Vi,
     Vi_ = Vi;
     yAccel_ = Xi + Vi * Ta_ + 0.5f * Ar_ * SQ(Ta_); // pos at end of accel phase
 
-    cout << "Xi: " << Xi << "\tXf: " << Xf << "\tVi: " << Vi << endl;
-    cout << "Amax: " << Amax << "\tVmax: " << Vmax << "\tDmax: " << Dmax << endl;
-    cout << "dX: " << dX << "\tdXst: " << dXstop << "\tdXmin: " << dXmin << endl;
-    cout << "Ar: " << Ar_ << "\tVr: " << Vr_ << "\tDr: " << Dr_ << endl;
-    cout << "Ta: " << Ta_ << "\tTv: " << Tv_ << "\tTd: " << Td_ << endl;
+    // cout << "Xi: " << Xi << "\tXf: " << Xf << "\tVi: " << Vi << endl;
+    // cout << "Amax: " << Amax << "\tVmax: " << Vmax << "\tDmax: " << Dmax << endl;
+    // cout << "dX: " << dX << "\tdXst: " << dXstop << "\tdXmin: " << dXmin << endl;
+    // cout << "Ar: " << Ar_ << "\tVr: " << Vr_ << "\tDr: " << Dr_ << endl;
+    // cout << "Ta: " << Ta_ << "\tTv: " << Tv_ << "\tTd: " << Td_ << endl;
 
     return true;
 }
@@ -137,60 +137,97 @@ TrapezoidalTrajectory::Step_t TrapezoidalTrajectory::eval(float t)
 
 using doctest::Approx;
 
-TEST_CASE("[planTrap]")
-{
+// TEST_CASE("[planTrap]")
+// {
+//     TrapezoidalTrajectory::Config_t config;
+//     TrapezoidalTrajectory trapTraj(config);
+
+//     float pos_range = 10000.0;
+//     float Vmax_range = 8000.0;
+//     float Amax_range = 10000.0;
+
+//     std::random_device rd;
+//     std::mt19937 gen(rd());
+//     std::uniform_real_distribution<float> posRandom(-pos_range, pos_range);
+//     std::uniform_real_distribution<float> velRandom(0.1 * Vmax_range, Vmax_range);
+//     std::uniform_real_distribution<float> accelRandom(0.1 * Amax_range, Amax_range);
+
+//     std::uniform_real_distribution<float> viChance(0, 1);
+
+//     cout << std::fixed;
+//     cout << std::setprecision(2);
+    
+//     for (int i = 0; i < 10000; i++)
+//     {
+//         auto Vmax = velRandom(gen);
+//         auto Amax = accelRandom(gen);
+//         auto Dmax = Amax;
+
+//         auto Xf = posRandom(gen);
+//         auto Xi = posRandom(gen);
+
+//         float Vi = 0.0f;
+//         std::uniform_real_distribution<float> viRandom(-Vmax * 1.5, Vmax * 1.5);
+//         if(viChance(gen) <= 0.5)
+//             Vi = viRandom(gen);
+//         else
+//             Vi = 0.0f;
+
+//         cout << endl;
+//         cout << "Test " << i+1 << endl;
+//         trapTraj.planTrapezoidal(Xf, Xi, Vi, Vmax, Amax, Dmax);
+        
+//         TrapezoidalTrajectory::Step_t step;
+
+//         step = trapTraj.eval(0.0f);
+//         CHECK(step.Y == Approx(Xi).epsilon(0.0001));
+//         CHECK(step.Yd == Approx(Vi).epsilon(0.0001));
+
+//         step = trapTraj.eval(trapTraj.Tf_ - 0.001);
+//         CHECK(step.Y == Approx(Xf-0.001*step.Yd).epsilon(0.01));
+//         CHECK(step.Yd == Approx(0.0f-Dmax*0.001).epsilon(0.01));
+
+//         step = trapTraj.eval(trapTraj.Tf_);
+//         CHECK(step.Y == Approx(Xf).epsilon(0.0001));
+//         CHECK(step.Yd == Approx(0.0f).epsilon(0.0001));
+//     }
+
+
+
+//     cout << "\n\n\n" << endl;
+// }
+
+TEST_CASE("handbrake"){
     TrapezoidalTrajectory::Config_t config;
     TrapezoidalTrajectory trapTraj(config);
 
-    float pos_range = 10000.0;
-    float Vmax_range = 8000.0;
-    float Amax_range = 10000.0;
 
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_real_distribution<float> posRandom(-pos_range, pos_range);
-    std::uniform_real_distribution<float> velRandom(0.1 * Vmax_range, Vmax_range);
-    std::uniform_real_distribution<float> accelRandom(0.1 * Amax_range, Amax_range);
 
-    std::uniform_real_distribution<float> viChance(0, 1);
+    float Xi = 7310.54;
+    float Xf = -9379.76;
+    float Vi = 3319.07;
 
-    cout << std::fixed;
-    cout << std::setprecision(2);
-    
-    for (int i = 0; i < 10000; i++)
-    {
-        auto Vmax = velRandom(gen);
-        auto Amax = accelRandom(gen);
-        auto Dmax = Amax;
+    config.accel_limit = 3711.41;
+    config.decel_limit = config.accel_limit;
+    config.vel_limit = 5055.92;
 
-        auto Xf = posRandom(gen);
-        auto Xi = posRandom(gen);
+    trapTraj.planTrapezoidal(Xf, Xi, Vi, config.vel_limit, config.accel_limit, config.decel_limit);
+    float dT = trapTraj.Tf_ / 1000.0f;
+    float time = 0;
 
-        float Vi = 0.0f;
-        std::uniform_real_distribution<float> viRandom(-Vmax * 1.5, Vmax * 1.5);
-        if(viChance(gen) <= 0.5)
-            Vi = viRandom(gen);
-        else
-            Vi = 0.0f;
-
-        cout << endl;
-        cout << "Test " << i+1 << endl;
-        trapTraj.planTrapezoidal(Xf, Xi, Vi, Vmax, Amax, Dmax);
-        
-        TrapezoidalTrajectory::Step_t step;
-
-        step = trapTraj.eval(0.0f);
-        CHECK(step.Y == Approx(Xi).epsilon(0.0001));
-        CHECK(step.Yd == Approx(Vi).epsilon(0.0001));
-
-        step = trapTraj.eval(trapTraj.Tf_ - 0.001);
-        CHECK(step.Y == Approx(Xf-0.001*step.Yd).epsilon(0.01));
-        CHECK(step.Yd == Approx(0.0f-Dmax*0.001).epsilon(0.01));
-
-        step = trapTraj.eval(trapTraj.Tf_);
-        CHECK(step.Y == Approx(Xf).epsilon(0.0001));
-        CHECK(step.Yd == Approx(0.0f).epsilon(0.0001));
+    float lastTime = 0.0f;
+    for(int i = 0; i < 1000; ++i){
+        auto step = trapTraj.eval(time);
+        cout << time+lastTime << "," << step.Y << "," << step.Yd << "," << step.Ydd << "\n";
+        if(i == 500){
+            lastTime = time;
+            Xi = step.Y;
+            Xf = 15000.0;
+            Vi = step.Yd;
+            trapTraj.planTrapezoidal(Xf, Xi, Vi, config.vel_limit, config.accel_limit, config.decel_limit);
+            dT = trapTraj.Tf_ / 500.0f;
+            time = 0.0f;
+        }
+        time += dT;
     }
-
-    cout << "\n\n\n" << endl;
 }
